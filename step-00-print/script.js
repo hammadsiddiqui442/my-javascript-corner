33a9f73d84586cfbdca6cc9d4000dc527aaa3636
Hammad Siddiqui <hammadsiddiqui442@gmail.com>
2022-04-05 00:13:29 +0500

    Added concatenation
    Added concatenation

--- step-00-print/script.js
+++ step-00-print/script.js
@@ -1,53 +1,58 @@
 console.log("Hammad")
-// // OUTPUT:
-// // Hammad
+// OUTPUT:
+// Hammad
 
 
 console.log("Hammad\nHammad")
-// // OUTPUT:
-// // Hammad
-// // Hammad
+// OUTPUT:
+// Hammad
+// Hammad
 
 
 console.log("Name = Hammad\nAge = 28\nCity = Karachi\nCountry = Pakistan")
-// // OUTPUT:
-// // Name = Hammad
-// // Age = 28
-// // City = Karachi
-// // Country = Pakistan
+// OUTPUT:
+// Name = Hammad
+// Age = 28
+// City = Karachi
+// Country = Pakistan
 
 
 console.log("Hammad Hammad")
-// // OUTPUT:
-// // Hammad Hammad
+// OUTPUT:
+// Hammad Hammad
 
 
 console.log("Hammad\tHammad")
-// // OUTPUT:
-// // Hammad  Hammad
+// OUTPUT:
+// Hammad  Hammad
 
 
 console.log("Hammad/tHammad")
-// // OUTPUT:
-// // Hammad/tHammad
+// OUTPUT:
+// Hammad/tHammad
 
 
 console.log("My name is\t'Hammad'")
-// // OUTPUT:
-// // My name is      'Hammad'
+// OUTPUT:
+// My name is      'Hammad'
 
 
 console.log("My name is\t \"Hammad\" ")
-// // OUTPUT:
-// // My name is       "Hammad"
+// OUTPUT:
+// My name is       "Hammad"
 
 
 console.log("My name is\t \"Hammad\" \\ ")
-// // OUTPUT:
-// // My name is       "Hammad" \
+// OUTPUT:
+// My name is       "Hammad" \
 
 
 console.log('My name is\t\'Hammad\'')
-// // OUTPUT:
-// // My name is      'Hammad'
+// OUTPUT:
+// My name is      'Hammad'
+
+
+console.log("Hammad" + "Hammad" + "Hammad") // Concatenation means plus strings
+// OUTPUT:
+// HammadHammadHammad
 
