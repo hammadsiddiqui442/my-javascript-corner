6435681c23a2792d71c01f6530d0012e98b667f6
Hammad Siddiqui <hammadsiddiqui442@gmail.com>
2022-04-04 23:58:23 +0500

    Completed step 00
    In this step 00, I learn print by console in so many different ways.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# my-javascript-corner
-This repo will contain all my JavaScript learnings and docs
+# My JavaScript Docs
+This repo will contain all my JavaScript learnings and docs.
+
+## Step-00 : Print
+- `\n` use for new line 
+- `\t` use for Tab
+- `/t` and `/n` reads as a word
+- you can not write `""` in the `""`
+- you can write single `''` and in which doule `""`
+- OR you can write as well this double `""` in which `''`
+
---
+++ step-00-print/script.js
@@ -0,0 +1,53 @@
+console.log("Hammad")
+// // OUTPUT:
+// // Hammad
+
+
+console.log("Hammad\nHammad")
+// // OUTPUT:
+// // Hammad
+// // Hammad
+
+
+console.log("Name = Hammad\nAge = 28\nCity = Karachi\nCountry = Pakistan")
+// // OUTPUT:
+// // Name = Hammad
+// // Age = 28
+// // City = Karachi
+// // Country = Pakistan
+
+
+console.log("Hammad Hammad")
+// // OUTPUT:
+// // Hammad Hammad
+
+
+console.log("Hammad\tHammad")
+// // OUTPUT:
+// // Hammad  Hammad
+
+
+console.log("Hammad/tHammad")
+// // OUTPUT:
+// // Hammad/tHammad
+
+
+console.log("My name is\t'Hammad'")
+// // OUTPUT:
+// // My name is      'Hammad'
+
+
+console.log("My name is\t \"Hammad\" ")
+// // OUTPUT:
+// // My name is       "Hammad"
+
+
+console.log("My name is\t \"Hammad\" \\ ")
+// // OUTPUT:
+// // My name is       "Hammad" \
+
+
+console.log('My name is\t\'Hammad\'')
+// // OUTPUT:
+// // My name is      'Hammad'
+
