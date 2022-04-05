c22f6307b99344d37485080006422389f374d2a8
Hammad Siddiqui <hammadsiddiqui442@gmail.com>
2022-04-05 22:17:43 +0500

    learning variables
    learning variables

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ This repo will contain all my JavaScript learnings and docs.
 - you can write single `''` and in which doule `""`
 - OR you can write as well this double `""` in which `''`
 
+## Step-01 : Variables
+- use `var` `name` and print by console
+- use `var` `name` and print in console by name three times by adding `+` `concatenation`
+- use `var` `name` and print name in console in three seprate lines by adding `\n`
+- use `var` `name` and print in console in three seprate line by adding `"\n"`
+- use `var` `myString`, `myInteger` `myFloat` `myBoolean`
+- use in `console` tpeof `(myString, myInteger, myFloat, myBoolean)`
---
+++ step-01-variables/script.js
@@ -0,0 +1,48 @@
+// var name = "Muhammad Hammad Siddiqui";
+// console.log(name)
+// // OUTPUT
+// // Muhammad Hammad Siddiqui
+
+
+// var name = "Hammad"
+// console.log(name + name + name)
+// // OUTPUT
+// // HammadHammadHammad
+
+
+// var name = "Muhammad Hammad Siddiqui\nMuhammad Hammad Siddiqui\nMuhammad Hammad Siddiqui"
+// console.log(name)
+// // OUTPUT
+// // Muhammad Hammad Siddiqui
+// // Muhammad Hammad Siddiqui
+// // Muhammad Hammad Siddiqui
+
+
+// var name = "Hammad";
+// console.log(name + "\n" + name + "\n" + name)
+// OUTPUT
+// Hammad
+// Hammad
+// Hammad
+
+
+// var name = "Hammad";
+// console.log(name, "\n", name, "\n", name)
+// OUTPUT
+// Hammad 
+//  Hammad
+//  Hammad
+
+
+var myString = "My name is Hammad"
+var myInteger = 28
+var myFloat = 3.142
+var myBoolean = true
+
+console.log(myString, myInteger, myFloat, myBoolean)
+console.log(typeof myString, typeof myInteger, typeof myFloat, typeof myBoolean)
+// OUTPUT
+// My name is Hammad 28 3.142 true
+// string number number boolean
+
+
